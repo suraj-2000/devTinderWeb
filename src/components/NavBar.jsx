d7c592c47f18b2e2e83fd4aa01fd731cd79c2d05
suraj-2000 <surajsingh907668@gmail.com>
2026-07-11 21:28:41 +0530

    fixed nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,7 +23,7 @@ const NavBar = () => {
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzt9giWjwNCackreb_tWA5drICRqkjo5ggAiSJ4ToZ1A&s";
 
   return (
-    <div className="navbar w-full bg-base-300 shadow-sm px-2">
+    <div className="navbar w-full bg-base-300 shadow-sm px-2 overflow-visible">
       <div className="flex-1 min-w-0">
         <Link
           to="/"
@@ -35,8 +35,8 @@ const NavBar = () => {
 
       <div className="flex items-center gap-2 shrink-0">
         {user && (
-          <div className="flex items-center min-w-0">
-            <p className="font-medium truncate max-w-[90px] sm:max-w-none">
+          <div className="flex items-center max-w-[180px] sm:max-w-none">
+            <p className="font-medium truncate">
               Welcome, {user.firstName}
             </p>
 
@@ -45,7 +45,7 @@ const NavBar = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="#1D9BF0"
-                className="w-4 h-4 ml-1 shrink-0"
+                className="w-5 h-5 ml-1 shrink-0"
               >
                 <path d="M12 2l2.09 2.26 3.07-.43.43 3.07L20 9l-2.41 2.1.43 3.07-3.07.43L12 17l-2.95-2.4-3.07-.43.43-3.07L4 9l2.41-2.1-.43-3.07 3.07.43L12 2zm-1 13l6-6-1.41-1.41L11 12.17l-2.59-2.58L7 11l4 4z" />
               </svg>
@@ -70,29 +70,28 @@ const NavBar = () => {
           <ul
             tabIndex={0}
             onClick={() => document.activeElement?.blur()}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <Link to="/profile" className="justify-between">
+              <Link to="/profile">
                 Profile
-                <span className="badge">New</span>
               </Link>
             </li>
 
             <li>
-              <Link to="/connections" className="justify-between">
+              <Link to="/connections">
                 Connection
               </Link>
             </li>
 
             <li>
-              <Link to="/requests" className="justify-between">
+              <Link to="/requests">
                 Request
               </Link>
             </li>
 
             <li>
-              <Link to="/premium" className="justify-between">
+              <Link to="/premium">
                 Premium
               </Link>
             </li>
